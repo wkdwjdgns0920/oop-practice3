fe02d999c7721a25d7dcf5da4d1bcb62116c16b9
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2024-08-09 15:46:19 +0900

    학점 계산기

---
+++ src/main/java/org/example/GradeCalculator.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.List;
+
+public class GradeCalculator {
+    private final List<Course> courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public double calculateGrade() {
+        // (학점수x교과목 평점)의 합계
+        double multipliedCreditAndCourseGrade = 0;
+        for (Course course : courses) {
+            multipliedCreditAndCourseGrade += course.getCredit() * course.getGradeToNumber();
+        }
+
+        // 수강신청 총학점 수
+        int totalCompletedCredit = courses.stream()
+                .mapToInt(Course::getCredit)
+                .sum();
+
+        return multipliedCreditAndCourseGrade / totalCompletedCredit;
+    }
+}
