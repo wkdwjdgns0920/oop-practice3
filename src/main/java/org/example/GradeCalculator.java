b87aef8199f671d2455b055e7856f9caa5e5d6aa
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2024-08-09 18:29:58 +0900

    일급 컬렉션 사용

--- src/main/java/org/example/GradeCalculator.java
+++ src/main/java/org/example/GradeCalculator.java
@@ -1,26 +1,27 @@
 package org.example;
 
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class GradeCalculator {
-    private final List<Course> courses;
+
+    private final Courses courses;
 
     public GradeCalculator(List<Course> courses) {
+        this.courses = new Courses(courses);
+    }
+
+    public GradeCalculator(Courses courses){
         this.courses = courses;
     }
 
     public double calculateGrade() {
         // (학점수x교과목 평점)의 합계
-        double multipliedCreditAndCourseGrade = 0;
-        for (Course course : courses) {
-            multipliedCreditAndCourseGrade += course.muliplyCreditAndCourseGrade();
-        }
+        double totalMultipliedCreditAndCouseGrade = courses.multiplyCreditAndCourseGrade();
 
         // 수강신청 총학점 수
-        int totalCompletedCredit = courses.stream()
-                .mapToInt(Course::getCredit)
-                .sum();
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
 
-        return multipliedCreditAndCourseGrade / totalCompletedCredit;
+        return totalMultipliedCreditAndCouseGrade / totalCompletedCredit;
     }
 }
--- src/test/java/org/example/GradeCalculatorTest.java
+++ src/test/java/org/example/GradeCalculatorTest.java
@@ -28,7 +28,7 @@ public class GradeCalculatorTest {
         List<Course> courses = List.of(new Course("OOP", 3, "A+"),
                 new Course("자료구조", 3,"A+"));
 
-        GradeCalculator gradeCalculator = new GradeCalculator(courses);
+        GradeCalculator gradeCalculator = new GradeCalculator(new Courses(courses));
         double gradeResult = gradeCalculator.calculateGrade();
 
         assertThat(gradeResult).isEqualTo(4.5);
